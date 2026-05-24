b9f3d3b4c8b69e9bf15488128a183484aede1e27
x0x-Lucifer-x0x <meetvachhani77@gmail.com>
2026-05-24 17:34:49 +0530

    fix : added prism gen to pkg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@prisma/client"],
+  serverExternalPackages: ["@prisma/client", "pg"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
